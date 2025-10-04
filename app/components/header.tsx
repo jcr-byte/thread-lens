7cded5a62bf12e9fb33d7e719df01b1be6e2a7ce
Snipe-ctrl <joshuarodli19@gmail.com>
2025-10-04 16:31:57 -0700

    Setup boilerplate header and outfits components

---
+++ app/components/header.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+export default function Header() {
+  return (
+    <div className="flex justify-center bg-white border-b border-gray-500">
+      <div className="p-5">
+        <h1 className="text-4xl font-semibold text-gray-900">Thread Lens</h1>
+      </div>
+    </div>
+  );
+}
+
---
+++ app/components/outfits.tsx
@@ -0,0 +1,12 @@
+
+import { Plus } from "lucide-react";
+
+export default function Outfits() {
+  return (
+    <div className="grid grid-cols-4 gap-4 px-20 py-10">
+        <div className="w-72 h-72 flex items-center justify-center rounded-lg bg-gray-200">
+            <Plus className="text-black w-50 h-50" />
+        </div>
+    </div>
+  );
+}
